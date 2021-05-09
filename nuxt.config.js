572e63bf507e4e51e6705da98a6dfe1e08530309
Favour Okeibunor <favour@hollatags.com>
2021-05-09 17:09:37 +0100

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   mode: 'universal',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'devjobs',
+    title: 'devgigs',
     htmlAttrs: {
       lang: 'en'
     },
