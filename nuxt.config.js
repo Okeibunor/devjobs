263c34a374e7ed3f8770b45faa19b4e9cfcf5caa
Favour Okeibunor <favour@hollatags.com>
2021-05-14 15:51:07 +0100

    fix responsiveness

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/vue-simple-markdown.js',
-    '~/plugins/vue-meta.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
